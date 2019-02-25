5426cfd6dda532b32e88fb47e77583a1d19ee6ed
avocadokek <47501889+avocadokek@users.noreply.github.com>
2019-02-25 18:45:54 +0200

    Add files via upload

--- NullableTypeExtensions.cs
+++ NullableTypeExtensions.cs
@@ -6,8 +6,17 @@ namespace PadawansTask10
     {
         public static bool IsNull(this object variable)
         {
-            // put your code here
+            if (variable==null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
+            throw new OutOfMemoryException();
+            throw new Exception();
         }
     }
 }
